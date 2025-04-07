e8acbfc52c0d08653816e7bb4931532e65e7514f
Lewys-Tech <wislerlewis58@gmail.com>
2025-04-07 06:52:12 +0300

    deploying a contract from another contract

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract SimpleStorage{
+
+    uint256  MyFavouriteNumber;
+
+    struct Person{
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavouriteNumber ;
+
+
+    function  store(uint256 _favouriteNumber) public{
+        MyFavouriteNumber = _favouriteNumber;
+    }
+
+    function retrieve() public view returns (uint256){
+        return MyFavouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+    listOfPeople.push(Person(_favouriteNumber, _name) );
+    
+    nameToFavouriteNumber[_name] = _favouriteNumber;
+    
+        
+    }
+    
+    }
+
